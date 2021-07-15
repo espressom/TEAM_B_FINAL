50557f349d577dd8f8fcf2631a66f6cbbbbcfce6
taez224 <graze248@gmail.com>
2021-07-15 16:48:12 +0900

    interceptor, logging

---
+++ Flow_0710ver/src/flow/mvc/interceptor/Interceptor.java
@@ -0,0 +1,39 @@
+package flow.mvc.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * @author 허태준 / 2021. 7. 15. / 오후 4:07:07
+ */
+public class Interceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("=========================preHandle executed");
+        HttpSession session = request.getSession();
+        Object obj = session.getAttribute("sessionID");
+          
+        if ( obj == null ){
+            // 로그인이 안되어 있는 상태임으로 로그인 폼으로 다시 돌려보냄(redirect)
+            response.sendRedirect("/Flow_0710ver/loginForm");
+            return false; 
+        }
+          
+        return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	
+}

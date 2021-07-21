2437e8fd6ed4dc21495112237f9b4be5e7ee7e8f
taez224 <graze248@gmail.com>
2021-07-21 18:59:58 +0900

    LOG 기능 추가

--- Flow_0710ver/src/flow/mvc/aop/Advice.java
+++ Flow_0710ver/src/flow/mvc/aop/Advice.java
@@ -2,6 +2,7 @@ package flow.mvc.aop;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
@@ -9,11 +10,14 @@ import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import flow.mvc.controller.member.MemberController;
+import flow.mvc.dao.log.LogDaoInter;
+import flow.mvc.vo.LogVO;
 
 /**
  * @author 허태준 / 2021. 7. 11. / 오후 9:06:16
@@ -21,42 +25,69 @@ import flow.mvc.controller.member.MemberController;
 @Component
 @Aspect
 public class Advice {
-	
-	private static final Log LOG = LogFactory.getLog( Advice.class );
-	  	
-	private void printParam(ProceedingJoinPoint pjp) {
+
+	private static final Log LOG = LogFactory.getLog(Advice.class);
+	@Autowired
+	private LogDaoInter logDaoInter;
+
+	private void printParam(ProceedingJoinPoint pjp, LogVO lvo) {
 		Object[] param = pjp.getArgs();
+		StringBuilder sb = new StringBuilder("");
 		if (param.length != 0) {
 			int length = param.length;
+
 			for (int i = 0; i < length; i++) {
-				LOG.info("| "+(i+1)+"번째 param: "+param[i]);
+				if (param[i] instanceof String) {
+					sb.append(param[i]);
+					sb.append("/");
+				}
+				LOG.info("| " + (i + 1) + "번째 param: " + param[i]);
 			}
 		}
+		lvo.setParams(sb.toString());
 	}
 
 	@Around(value = "execution(* flow.mvc.controller.*.*.*(..))")
 	public Object logAdvice(ProceedingJoinPoint pjp) throws Throwable {
 
+		LogVO lvo = new LogVO();
 		String methodName = pjp.getSignature().getName();
+		lvo.setRequest(methodName);
 		String[] classNames = pjp.getTarget().getClass().getCanonicalName().split("\\.");
-		String className = classNames[classNames.length-1];		
-		HttpSession session = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest().getSession();
+		String className = classNames[classNames.length - 1];
 		LOG.info("=================================================");
-		LOG.info("| [LOG]  "+className+" :: " + methodName + " 호출");
-		String sessionID = (String) session.getAttribute("sessionID");
-		if ( sessionID != null)
-		LOG.info("| [B Y]  "+ sessionID);
-		printParam(pjp);
+		LOG.info("| [LOG]  " + className + " :: " + methodName + " 호출");
+		lvo = getLogInfo(lvo);
+		if (lvo.getM_id() != null) {
+			LOG.info("| [B Y]  " + lvo.getM_id());
+		}
+		printParam(pjp, lvo);
 		Object rev = pjp.proceed();
 		if (rev != null) {
 			LOG.info("| returnv :: " + rev);
 		}
-		printParam(pjp);
-		LOG.info("| [LOG]  "+className+" :: " + methodName + " 종료");
-		sessionID = (String) session.getAttribute("sessionID");
-		if ( sessionID != null)
-		LOG.info("| [B Y]  "+ sessionID);
+		printParam(pjp, lvo);
+		LOG.info("| [LOG]  " + className + " :: " + methodName + " 종료");
+		lvo = getLogInfo(lvo);
+		if (lvo.getM_id() != null) {
+			LOG.info("| [B Y]  " + lvo.getM_id());
+		}
 		LOG.info("=================================================");
+		if (lvo.getM_id() == null) {
+			lvo.setM_id("");
+		}
+		logDaoInter.addlog(lvo);
 		return rev;
+	}
+
+	private LogVO getLogInfo(LogVO lvo) {
+		HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes()))
+				.getRequest();
+		lvo.setM_ip(request.getRemoteAddr());
+		lvo.setUagent(request.getHeader("User-Agent"));
+		HttpSession session = request.getSession();
+		String m_id = (String) session.getAttribute("sessionID");
+		lvo.setM_id(m_id != null ? m_id : lvo.getM_id()); 
+		return lvo;
 	}
 }
---
+++ Flow_0710ver/src/flow/mvc/dao/log/LogDao.java
@@ -0,0 +1,24 @@
+package flow.mvc.dao.log;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import flow.mvc.vo.LogVO;
+
+/**
+ * @author 허태준 / 2021. 7. 21. / 오후 3:20:30
+ */
+@Repository
+public class LogDao implements LogDaoInter{
+	
+	@Autowired
+	private SqlSessionTemplate ss;
+
+
+	@Override
+	public void addlog(LogVO lvo) {
+		ss.insert("log.addlog", lvo);
+	}
+
+}
